bd652dcd153286e1fca95e42203967aa860c710e
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-08-28 17:24:47 +0530

    Add gh pages

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -15,7 +15,11 @@ const MovieDetail = ({ movie }) => {
 						<div className="col-md-10">
 							<div className="card">
 								<div className="card-body">
-									<h3 className="card-title">{movie.original_title}</h3>
+									<h3 className="card-title">
+										{movie.media_type === "movie"
+											? movie.original_title
+											: movie.original_name}
+									</h3>
 									<h6 className="card-subtitle mb-2 text-muted">
 										{movie.release_date}
 									</h6>
